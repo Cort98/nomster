d146dbdb9e9f942e5e24f9e531521fe3f4a73fca
Cort Ferguson <98cort@gmail.com>
2016-06-10 06:09:10 +0000

    Validating comments and showing error for invalid data entrys for photo and comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,9 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @photo = @place.photos.create(photo_params)
+    if @photo.invalid?
+      flash[:error] = '<strong>Could not save photo</strong>, the data you entered is invalid.'
+    end
     redirect_to place_path(@place)
   end
   
