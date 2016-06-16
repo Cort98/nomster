1e7d585aafbf306dcf513c6d10b42f5150618403
Cort Ferguson <98cort@gmail.com>
2016-06-16 19:35:22 +0000

    Adding user controller, route, and show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Nomster::Application.routes.draw do
     resources :comments, only: :create
     resources :photos, only: [:create, :destroy, :show]
   end
+  resources :users, only: :show
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   
